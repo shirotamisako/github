50ff0a617d906b9de6dd4c88123781ab30156b16
misako shirota <Misako@s-misako-shirotano-macbook-pro.local>
2017-01-12 19:40:10 +0900

    Initial Commit

---
+++ github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github
+//
+//  Created by misako shirota on 2017/01/12.
+//  Copyright © 2017年 misako shirota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
